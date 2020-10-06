9721fb667def9f5f01163b2e4c64b0fed1ce2ca9
nebelgrau <nebelgrau@gmx.com>
2020-10-06 08:55:02 +0200

    added shell context

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 from flask_script import Manager
 
-from my_app import app
+from my_app import app, db, Car
 
 manager = Manager(app)
 app.config['DEBUG'] = True
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, Car=Car)
 
 if __name__ ==   '__main__':
     manager.run()
