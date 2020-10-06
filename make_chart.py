4934aff59e06cbc0ede86004cf1914dc14800cc4
Michal Lasak <u076079@fiatauto.adfa.local>
2020-10-06 15:33:10 +0200

    some cleanup, added docstrings

--- make_chart.py
+++ make_chart.py
@@ -3,16 +3,17 @@ import io
 import base64
 
 def make_chart(x, y, param):
- 
-    fig, ax = plt.subplots(figsize = (10,6))
-    ax.bar(x, y, color = '#3C99DC')
-    ax.set_title('average {}'.format(param))
-    img = io.BytesIO()
-    fig.savefig(img, format = 'png')
-    img.seek(0)
-    chart = base64.b64encode(img.getvalue()).decode()
-    plt.close()
+	'''make a bar chart with supplied data, return a formatted URL'''
+  
+	fig, ax = plt.subplots(figsize = (10,6))
+	ax.bar(x, y, color = '#3C99DC')
+	ax.set_title('average {}'.format(param))
+	img = io.BytesIO()
+	fig.savefig(img, format = 'png')
+	img.seek(0)
+	chart = base64.b64encode(img.getvalue()).decode()
+	plt.close()
 
-    chart_url = 'data:image/png;base64,{}'.format(chart)
+	chart_url = 'data:image/png;base64,{}'.format(chart)
 
-    return chart_url
+	return chart_url
--- my_app.py
+++ my_app.py
@@ -14,75 +14,77 @@ database_file = 'sqlite:///{}'.format(os.path.join(project_dir, 'cars.db'))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file # tell the app where the database is
 
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app) # initialize Bootstrap
 
 db = SQLAlchemy(app) # initialize a connection to the database
 
-class Car(db.Model):
-    __tablename__ = 'cars'
-    index = db.Column(db.Integer(), unique = True, nullable=False, primary_key=True)
-    brand = db.Column(db.String(40), unique = False, nullable = False)
-    model = db.Column(db.String(40), unique = False, nullable = True)
-    model_year = db.Column(db.Integer(), unique = False, nullable=False)
-    origin = db.Column(db.String(80), unique = False, nullable = True)
-    cylinders = db.Column(db.Integer(), unique = False, nullable=False)
-    displacement_ccm = db.Column(db.Float(), unique = False, nullable=False)    
-    horsepower = db.Column(db.Integer(), unique = False, nullable=False)
-    acceleration = db.Column(db.Float(), unique = False, nullable=False)
-    weight_kg = db.Column(db.Float(), unique = False, nullable=False)
-    liters_per_100km = db.Column(db.Float(), unique = False, nullable=False)
+class Car(db.Model):	
+	__tablename__ = 'cars'
+	index = db.Column(db.Integer(), unique = True, nullable=False, primary_key=True)
+	brand = db.Column(db.String(40), unique = False, nullable = False)
+	model = db.Column(db.String(40), unique = False, nullable = True)
+	model_year = db.Column(db.Integer(), unique = False, nullable=False)
+	origin = db.Column(db.String(80), unique = False, nullable = True)
+	cylinders = db.Column(db.Integer(), unique = False, nullable=False)
+	displacement_ccm = db.Column(db.Float(), unique = False, nullable=False)    
+	horsepower = db.Column(db.Integer(), unique = False, nullable=False)
+	acceleration = db.Column(db.Float(), unique = False, nullable=False)
+	weight_kg = db.Column(db.Float(), unique = False, nullable=False)
+	liters_per_100km = db.Column(db.Float(), unique = False, nullable=False)
 
-    def __repr__(self):
-        return "<Brand: {}, Model: {}, Origin: {}>".format(self.brand, self.model, self.origin)
+	def __repr__(self):
+		return "<Brand: {}, Model: {}, Origin: {}>".format(self.brand, self.model, self.origin)
 
 @app.route('/')
 def home():
-
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/select_brand/')
 def select_brand():
-    brands = db.session.query(Car.brand.distinct()).all()
-    brands = sorted([brand[0] for brand in brands])
-    
-    return render_template('select_brand.html', brands = brands)
+	'''select brand from a list'''
+	brands = db.session.query(Car.brand.distinct()).all()
+	brands = sorted([brand[0] for brand in brands]) # list of all brands in the 'brand' column    
+	return render_template('select_brand.html', brands = brands)
 
 @app.route('/select_brand/display')
 def by_brand():
-    
-    brand = request.args.get('brand')
-    
-    avg_weight = db.session.query(func.avg(Car.weight_kg)).filter(Car.brand == brand).scalar()
-    avg_horsepower = db.session.query(func.avg(Car.horsepower)).filter(Car.brand == brand).scalar()
-    avg_consumption = db.session.query(func.avg(Car.liters_per_100km)).filter(Car.brand == brand).scalar()
-    count = db.session.query(func.count(Car.index)).filter(Car.brand == brand).scalar()
-
-    return render_template('by_brand.html', brand = brand, avg_weight = avg_weight, avg_horsepower = avg_horsepower, avg_consumption = avg_consumption, count = count)
+	'''retrieve data from the database with a query based on the selected brand'''
+	brand = request.args.get('brand') # brand selected by the user
+	avg_weight = db.session.query(func.avg(Car.weight_kg)).filter(Car.brand == brand).scalar()
+	avg_horsepower = db.session.query(func.avg(Car.horsepower)).filter(Car.brand == brand).scalar()
+	avg_consumption = db.session.query(func.avg(Car.liters_per_100km)).filter(Car.brand == brand).scalar()
+	count = db.session.query(func.count(Car.index)).filter(Car.brand == brand).scalar()
+	
+	return render_template('by_brand.html', brand = brand, avg_weight = avg_weight, avg_horsepower = avg_horsepower, avg_consumption = avg_consumption, count = count)
 
 @app.route('/select_year/')
 def select_year():
-    years = db.session.query(Car.model_year.distinct()).all()
-    years = sorted([year[0] for year in years])
-    return render_template('select_year.html', years = years)
+	'''select year from a list'''
+	years = db.session.query(Car.model_year.distinct()).all()
+	years = sorted([year[0] for year in years]) # all year values in the 'model_year' column
+	return render_template('select_year.html', years = years)
 
 @app.route('/select_year/display')
 def by_year():
-    year = request.args.get('year')
+	'''retrieve data from the database with a query based on the selected model year'''
+	
+	year = request.args.get('year') # year chosen by the user
 
-    avg_HP = db.session.query(func.avg(Car.horsepower)).filter(Car.model_year == year).scalar()
-    return render_template('by_year.html', year = year, avg_HP = avg_HP)
+	avg_HP = db.session.query(func.avg(Car.horsepower)).filter(Car.model_year == year).scalar()
+	return render_template('by_year.html', year = year, avg_HP = avg_HP)
 
 @app.route('/test_chart')
 def test_chart():       
-    
-    val = request.args.get('val')
+	'''generate a chart with data retrieved from the database'''
+	
+	val = request.args.get('val') # value chosen by the user
    
-    queries = {'weight': Car.weight_kg, 'horsepower': Car.horsepower}
+	queries = {'weight': Car.weight_kg, 'horsepower': Car.horsepower} # possible values 
 
-    myquery = db.session.query(Car.model_year, func.avg(queries.get(val, Car.weight_kg))).group_by(Car.model_year).all()
+	myquery = db.session.query(Car.model_year, func.avg(queries.get(val, Car.weight_kg))).group_by(Car.model_year).all()
 
-    years = [item[0] for item in myquery]
-    values = [item[1] for item in myquery]
-    chart = make_chart(years, values, val)
+	years = [item[0] for item in myquery]
+	values = [item[1] for item in myquery]
+	chart = make_chart(years, values, val)
 
-    return render_template('test_chart.html', vals = queries.keys(), chart = chart)
+	return render_template('test_chart.html', vals = queries.keys(), chart = chart)
